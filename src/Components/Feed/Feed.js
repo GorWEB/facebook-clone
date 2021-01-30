5f84c18d687056178432307876c36d914d92b225
GorWEB <brutyan.99@mail.ru>
2021-01-30 16:08:43 +0000

    Fixed bugs and warnings

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -16,14 +16,14 @@ function Feed(props) {
         <Post
           profileSrc={user.photoURL}
           postText="Post text"
-          image="https://scontent.fevn2-1.fna.fbcdn.net/v/t1.0-9/s960x960/117042020_2599283503670277_2336013943808384023_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=0V93yhwB9XEAX-g2dHd&_nc_ht=scontent.fevn2-1.fna&tp=7&oh=684f7f80e8c9347b403e0b939022a167&oe=5FAA0153"
+          image="https://scontent.fevn6-1.fna.fbcdn.net/v/t1.0-9/142561113_2745283525736940_8948690310845445948_o.jpg?_nc_cat=100&ccb=2&_nc_sid=8bfeb9&_nc_ohc=4zsIrdxAA0oAX_0_0h5&_nc_ht=scontent.fevn6-1.fna&oh=a405caf6ec0c82af064346aac2bd6d3d&oe=603ABC7C"
           userName={user.displayName}
           timestamp={new Date().toUTCString()}
         />
         <Post
           profileSrc={user.photoURL}
           postText="Post text"
-          image="https://scontent.fevn2-1.fna.fbcdn.net/v/t1.0-9/s960x960/117042020_2599283503670277_2336013943808384023_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=0V93yhwB9XEAX-g2dHd&_nc_ht=scontent.fevn2-1.fna&tp=7&oh=684f7f80e8c9347b403e0b939022a167&oe=5FAA0153"
+          image="https://scontent.fevn6-1.fna.fbcdn.net/v/t1.0-9/117042020_2599283503670277_2336013943808384023_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=p8cdFwuMvcYAX9s1w8m&_nc_ht=scontent.fevn6-1.fna&oh=e106aaef31e81953fb97c139e3d7e43f&oe=603A746B"
           userName={user.displayName}
           timestamp={new Date().toUTCString()}
         />
